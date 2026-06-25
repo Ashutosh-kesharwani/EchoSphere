46b0497b85fc8c4c860708027f0cfd11c4e9e002
Ashutosh Kesharwani <ashutoshkesharwani20021406@gmail.com>
2026-06-25 21:07:43 +0530

    Added aggregate plugin in video Schema and update .gitignore file

--- backend/src/models/video.model.js
+++ backend/src/models/video.model.js
@@ -1,43 +1,48 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new mongoose.Schema({
-    videoFile : {
-        type: String, // cloudinary url
-        required:[true,"Video is Required"]
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: [true, "Video is Required"],
     },
-    thumbnail:{
-        type:String, // cloudinary url
-        required:[true,"Thumbnail is Required"]
+    thumbnail: {
+      type: String, // cloudinary url
+      required: [true, "Thumbnail is Required"],
     },
-    owner:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"User",
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    title:{
-        type:String,
-        required:[true,"Title is Required"],
-        lowercase:true,
-        trim:true,
+    title: {
+      type: String,
+      required: [true, "Title is Required"],
+      lowercase: true,
+      trim: true,
     },
-    description:{
-        type:String,
-        trim:true,
+    description: {
+      type: String,
+      trim: true,
     },
-    duration:{
-        type:Number, // cloudinary se as cloudinary file upload ke baad kaafi info bhejta hai
-        default:0,
+    duration: {
+      type: Number, // cloudinary se as cloudinary file upload ke baad kaafi info bhejta hai
+      default: 0,
     },
-    views:{
-        type:Number,
-        default:0,
+    views: {
+      type: Number,
+      default: 0,
     },
-    isPublished:{
-        type:Boolean,
-        default:false,
+    isPublished: {
+      type: Boolean,
+      default: false,
     },
-},{timestamps:true});
+  },
+  { timestamps: true }
+);
 
+// we use aggregate paginate as a plugin in our schema
+videoSchema.plugin(mongooseAggregatePaginate);
 
-
-const Video = mongoose.model("Video",videoSchema);
+const Video = mongoose.model("Video", videoSchema);
 export default Video;
